d925eb6ce52e24bb8b8253803b38277dd8ae8aa8
trinada <31551767+trinada@users.noreply.github.com>
2019-03-03 16:56:58 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "this is a demo ");
+		get("/hello", (req, res) -> " this is a test for pole scm demo ");
 	}
 }
